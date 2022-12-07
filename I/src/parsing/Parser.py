16077ca23367c4359177e3f4feec5fa01d6df894
relatev <joshua@familiewirtz.de>
2022-12-07 16:39:33 +0100

    I: Parser: Fix grammar path handling.

--- I/src/parsing/Parser.py
+++ I/src/parsing/Parser.py
@@ -3,19 +3,23 @@ from LabeledTransitionSystem import LabeledTransitionSystem
 from parsing.LabelledTransitionSystemTransformer import LabelledTransitionSystemTransformer
 from parsing.FormulaTransformer import FormulaTransformer
 
-def parse_system(path):
-    aut_parser = Lark.open("src/grammars/aut.lark", parser="lalr")
+import os
+
+grammars_dir = os.path.join(os.path.dirname(__file__), 'grammars')
+
+def parse_system(system_path):
+    aut_parser = Lark.open(os.path.join(grammars_dir, 'aut.lark'), parser="lalr")
         
-    system = aut_parser.parse(open(path).read())
+    system = aut_parser.parse(open(system_path).read())
         
     lts = LabelledTransitionSystemTransformer().transform(system)
 
     return lts
 
-def parse_formula(path):
-    mcf_parser = Lark.open("src/grammars/mcf.lark", parser="lalr")
+def parse_formula(formula_path):
+    mcf_parser = Lark.open(os.path.join(grammars_dir, 'mcf.lark'), parser="lalr")
 
-    formula = mcf_parser.parse(open(path).read())
+    formula = mcf_parser.parse(open(formula_path).read())
 
     mu = FormulaTransformer().transform(formula)
     
