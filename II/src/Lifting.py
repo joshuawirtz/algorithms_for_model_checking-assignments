16d572e6ff45afe647a37a42a8390009f286f640
DAAN BOELHOUWERS <daanboelhouwers@gmail.com>
2023-01-09 09:31:21 +0100

    Implement SuccessorLifting

--- II/src/Lifting.py
+++ II/src/Lifting.py
@@ -17,3 +17,18 @@ class RandomLifting(Lifting):
         shuffled = random.sample(self.game.vertices,
                                   len(self.game.vertices))
         return iter(shuffled)
+
+class SuccessorLifting(Lifting):
+
+    def __init__(self, game):
+        super().__init__(game)
+        successors = {v.identifier: 0 for v in self.game.vertices}
+        for v in self.game.vertices:
+            for s in v.successors:
+                successors[s] += 1
+        self.succesors = successors
+
+    def __iter__(self):
+        return iter(sorted(self.game.vertices, key=lambda v: self.succesors[v.identifier]))
+
+# First lift the vertices with selfloops
